4d466024c68d56aac081a286fcfcba318bc1332a
ohtrobinson <52220544+ohtrobinson@users.noreply.github.com>
2019-10-08 11:43:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
   url = 'https://https://github.com/ohtrobinson/run_length_encoding',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/ohtrobinson/run_length_encoding/archive/V_01.tar.gz',    # I explain this later on
   keywords = ['RLE', 'compress', 'decompress'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  #install_requires=[            # I get to this in a second
+   #       'validators',
+    #      'beautifulsoup4',
+    #  ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
