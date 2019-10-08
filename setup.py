1e7c02a56004340a0d30938e446734268053661f
ohtrobinson <52220544+ohtrobinson@users.noreply.github.com>
2019-10-08 11:42:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Ollie Robinson',                   # Type in your name
   author_email = 'ohtrobinson@gmail.com',      # Type in your E-Mail
   url = 'https://https://github.com/ohtrobinson/run_length_encoding',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ohtrobinson/run_length_encoding/archive/V_01.tar.gz',    # I explain this later on
   keywords = ['RLE', 'compress', 'decompress'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
